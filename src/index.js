5afebe999a0fbaa076a9734febae3ca91cba41d7
Hassam Aslam <hassam.aslam@codistan.org>
2022-02-24 16:15:11 +0500

    plugin name updated

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import pkg from "../package.json";
  */
 export default async function register(app) {
   await app.registerPlugin({
-    label: "Plugin Example",
-    name: "plugin-example",
+    label: "opulent Door plugin",
+    name: "opulent-door-plugin",
     version: pkg.version
   });
 }
